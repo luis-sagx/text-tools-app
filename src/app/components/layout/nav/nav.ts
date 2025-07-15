461451b11f619e403fa4bdf78b411179d70e9fd4
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-07-15 07:49:32 -0500

    responsive-na

--- src/app/components/layout/nav/nav.ts
+++ src/app/components/layout/nav/nav.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  isMenuOpen: boolean = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
